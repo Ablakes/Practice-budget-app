1f512ddba0f7c66bf657f141fc6938c87927ed41
Ablakes <blakesley.alex@gmail.com>
2018-06-20 22:21:04 -0700

    add new item to HTML complete

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var UIController = (function () {
         type: '.add__type',
         description: '.add__description',
         value: '.add__value',
-        addBtn: '.add__btn'
+        addBtn: '.add__btn',
+        incomeList: '.income__list',
+        expensesList: '.expenses__list'
     };
 
     return {
@@ -11,12 +13,33 @@ var UIController = (function () {
             return {
                 type: document.querySelector(DOMstrings.type).value,
                 description: document.querySelector(DOMstrings.description).value,
-                value: document.querySelector(DOMstrings.value).value
+                value: document.querySelector(DOMstrings.value).value,
             };
+        },
+        addListItem: function (obj, type) {
+            //Create html
+            var html, newHTML, element;
+
+            if (type == 'inc') {
+                html = '<div class="item clearfix" id="income-IDDD"><div class="item__description">DESCCC</div><div class="right clearfix"><div class="item__value">VALLL</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                element = DOMstrings.incomeList;
+            } else if (type === 'exp') {
+                html = '<div class="item clearfix" id="expense-IDDD"><div class="item__description">DESCCC</div><div class="right clearfix"><div class="item__value">VALLL</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                element = DOMstrings.expensesList;
+            }
+
+            //Sub in the correct info for class names and whatnot. 
+            //WTF is happening here you ask? Well, we need to create a copy of the html for each new item. We can't alter the html variable because then we wouldn't be able to reuse it in this context. So, newHTML becomes the placeholder for each new item until it is pushed into the DOM.
+            newHTML = html.replace('IDDD', obj.id);
+            newHTML = newHTML.replace('DESCCC', obj.description);
+            newHTML = newHTML.replace('VALLL', obj.value);
+
+            // insert into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
         },
         getDOMstrings: function () {
             return DOMstrings;
-        },
+        }
     };
 
 
@@ -66,7 +89,7 @@ var budgetController = (function () {
                 newItem = new Income(ID, des, val);
             }
             data.allItems[type].push(newItem);
-            // console.log(data.allItems.inc);
+            return newItem;
         },
 
         testing: data
@@ -85,7 +108,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     function addEventListeners() {
         document.querySelector(DOM.addBtn).addEventListener('click', function () {
             ctrlAddItem();
-            console.log(budgetController.testing.allItems);
+            // console.log(budgetController.testing.allItems);
         });
         document.addEventListener('keypress', function (event) {
             if (event.keyCode === 13 || event.which === 13 /* event.which is for older browsers) */ ) {
@@ -97,8 +120,11 @@ var controller = (function (budgetCtrl, UICtrl) {
 
 
     var ctrlAddItem = function () {
+        var newItem;
         var input = UICtrl.getInput();
-        budgetCtrl.addItem(input.type, input.description, input.value);
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        console.log(newItem);
+        UICtrl.addListItem(newItem, input.type);
     }
 
     return {
